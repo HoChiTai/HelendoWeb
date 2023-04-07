11349b570a8106f701dd0bc56580f060d4190eca
Hồ Chí Tài <hochitai24@gmail.com>
2023-04-07 16:30:10 +0700

    initial project

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace HelendoWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace HelendoWeb.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
